609cded770d657064d508312124d24450c77c242
ekqiplur <ekqiplur@gmail.com>
2025-07-04 20:24:55 +0700

    Revision

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
-# Rename hasil extract
 mv clang slim-clang
 cd slim-clang
 
-# Bersihkan file tidak penting
+# Hapus file besar yang tidak wajib untuk compiling kernel
 rm -rf libexec/ share/ source.properties ndk-metadata
+rm -f lib64/libclang*.so*         # clang-cpp, libclang
+rm -f lib64/liblldb*              # debugger
+rm -f bin/clang-check             # tool helper
+rm -f bin/lld                     # optional
+rm -f bin/clang-14                # kita bisa pakai symlink `clang`
+
+# Strip semua binary jika bisa
 find bin -type f -exec strip --strip-unneeded {} \; || true
 find lib -name '*.a' -delete || true
